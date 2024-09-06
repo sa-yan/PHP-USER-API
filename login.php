a7034cd1cddfa2d09b641d7279ba3af1d6d4698c
Sayan Mondal <sayan.mondal4557@gmail.com>
2024-09-06 23:54:13 +0530

    Updated Register and Login Page

--- login.php
+++ login.php
@@ -2,19 +2,40 @@
 
 require "connection.php";
 
-$email = $_POST["email"];
-$password = $_POST["password"];
+if (isset($_POST["email"]) && isset($_POST["password"])) {
 
-$checkUser = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    
+    // Check if the user exists with the given email
+    $checkUser = "SELECT * FROM user WHERE email='$email'";
+    $result = mysqli_query($conn, $checkUser);
 
-$result = mysqli_query($conn, $checkUser);
+    if(mysqli_num_rows($result) > 0){
+        $user = mysqli_fetch_assoc($result);
+        $storedHashedPassword = $user["password"];
 
-if (mysqli_num_rows($result) > 0) {
-    $reponse['error']=false;
-    $reponse['message']="Login Successful";
-}else{
-    $reponse['error']=true;
-    $reponse['message']="No account is associated with this email";
+        // Verify the entered password with the hashed password from the database
+        if(password_verify($password, $storedHashedPassword)){
+            $response['error'] = false;
+            $response['message'] = "Login Successful";
+            $response['username'] = $user['username'];
+            $response['email'] = $user['email'];
+            $response['id'] = $user['id'];
+        } else {
+            $response['error'] = true;
+            $response['message'] = "Incorrect password";
+        }
+    } else {
+        $response['error'] = true;
+        $response['message'] = "No account is associated with this email";
+    }
+    
+} else {
+    $response['error'] = true;
+    $response['message'] = "All fields are required!";
 }
 
-echo json_encode($reponse);
+echo json_encode($response);
+
+mysqli_close($conn);
--- register.php
+++ register.php
@@ -3,38 +3,60 @@
 require "connection.php";
 require "sendMail.php";
 
-$userName = $_POST["username"];
-$email = $_POST["email"];
-$password = $_POST["password"];
+$response = array();
 
-if(empty($userName) && empty($email)&& empty($password)){
-    $response['error'] = true;
-    $response['message'] = "All fields are mandatory";
-    exit;
-}
+if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"])) {
 
+    $userName = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-$checkUser = "SELECT * FROM user WHERE email='$email'";
-$checkQuery = mysqli_query($conn, $checkUser);
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-if (mysqli_num_rows($checkQuery) > 0) {
-    $response['error'] = true;
-    $response['message'] = "User Already Exists";
-} else {
-
-    $insertQuery = "INSERT INTO user(username, email, password)
-    VALUES('$userName', '$email', '$password')";
-
-    $result = mysqli_query($conn, $insertQuery);
-    if ($result) {
-        sendMail($email, $userName);
-        $response['error'] = false;
-        $response['message'] = "Registration successful";
+    if (empty($userName) && empty($email) && empty($password)) {
+        $response["error"] = true;
+        $response["message"] = "All fields are required !";
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $response["error"] = true;
+        $response["message"] = "Please provide a valid email adress";
+    } else if (strlen($password) < 5) {
+        $response["error"] = true;
+        $response["message"] = "The password must consist of a minimum of 6 digits.";
     } else {
-        $response['error'] = true;
-        $response['message'] = "Registration Failed!";
+        $checkUser = "SELECT * FROM user WHERE email='$email'";
+        $checkQuery = mysqli_query($conn, $checkUser);
+
+        if (mysqli_num_rows($checkQuery) > 0) {
+            $response['error'] = true;
+            $response['message'] = "User Already Exists";
+        } else {
+
+            $insertQuery = "INSERT INTO user(username, email, password)
+        VALUES('$userName', '$email', '$hashedPassword')";
+
+            $result = mysqli_query($conn, $insertQuery);
+
+            if ($result) {
+
+                $userId = mysqli_insert_id($conn);
+
+                sendMail($email, $userName);
+                $response['error'] = false;
+                $response['message'] = "Registration successful";
+                $response['username'] = $userName;
+                $response['email'] = $email;
+                $response['id'] = $userId;
+            } else {
+                $response['error'] = true;
+                $response['message'] = "Registration Failed!";
+            }
+        }
     }
+} else {
+    $response['error'] = true;
+    $response['message'] = "Registration Failed!" + mysqli_error($conn);
 }
+
 
 echo json_encode($response);
 
